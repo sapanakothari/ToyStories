c350e65169597fdc589ff60429210f11031d1c93
sapanakothari <sapanavkothari@gmail.com>
2017-11-10 15:11:14 +0530

    Update Item.scala

--- Item.scala
+++ Item.scala
@@ -3,13 +3,14 @@ case class Item(name:String, unitPrice:Double){
 
 	def price(qty:Int):Double={
 		Offer.get(name) match{
-			case Some(offer) if qty>=offer.minQty=> (qty/offer.minQty)*offer.price(unitPrice) + price(qty%offer.minQty)
+			case Some(offer) if qty>=offer.minQty=>
+				(qty/offer.minQty)*offer.price(unitPrice) + price(qty%offer.minQty)
 			case _ => fullPrice(qty)
 		}
 	}
 }
 
 object Item{
-val inventory = Set(Item("Dove",30),Item("Axe",100))
-def get(name:String) = inventory.collectFirst{case i@Item(`name`,_)=>i}
+	val inventory = Set(Item("Dove",30),Item("Axe",100))
+	def get(name:String) = inventory.collectFirst{case i@Item(`name`,_)=>i}
 }
