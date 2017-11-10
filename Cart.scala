b5fcd2260d1c5c70bc7ce7f88dcabdc372c173f7
sapanakothari <sapanavkothari@gmail.com>
2017-11-10 15:12:17 +0530

    Update Cart.scala

--- Cart.scala
+++ Cart.scala
@@ -1,16 +1,15 @@
 case class CartItem(item:Item,qty:Int,totalPrice:Double,discount:Double)
 
 class Cart(){
-var items:List[CartItem] = List()
+	var items:List[CartItem] = List()
 
-def add(name:String,qty:Int):Boolean ={
-	val item = Item.get(name).get
-	val actualPrice = item.price(qty)
-	val discount = item.fullPrice(qty) - actualPrice
-	items = CartItem(item,qty,actualPrice,discount)::items
-	true
-}
+	def add(name:String,qty:Int):Boolean ={
+		val item = Item.get(name).get
+		val actualPrice = item.price(qty)
+		val discount = item.fullPrice(qty) - actualPrice
+		items = CartItem(item,qty,actualPrice,discount)::items
+		true
+	}
 
-def price = items.map(_.totalPrice).sum
-
+	def price = items.map(_.totalPrice).sum
 }
