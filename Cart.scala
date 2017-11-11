bbb7477868048d394eb1da3c941320bba61c4e2c
sapanakothari <sapanavkothari@gmail.com>
2017-11-11 11:06:09 +0530

    Update Cart.scala

--- Cart.scala
+++ Cart.scala
@@ -1,15 +1,56 @@
-case class CartItem(item:Item,qty:Int,totalPrice:Double,discount:Double)
-
-class Cart(){
+class Cart{
+	case class CartItem(item:Item,qty:Int,totalPrice:Double,discount:Double)
 	var items:List[CartItem] = List()
+	private var cartDiscount:Double=1
+
+	def price = items.map(_.totalPrice).sum
+
+	def applyOffer(){
+		val offers = CartOffer.get(this)
+		for(offer<-offers){
+			val offerDiscount = (100-offer.discount)/100
+			if(offerDiscount !=cartDiscount){
+				cartDiscount = offerDiscount 
+				refresh	
+			} 
+		}
+	}
+
+	def refresh(){
+		var cartItems:List[CartItem] = for(cartItem <- items) yield addItem(cartItem.item.name,cartItem.qty)
+		items = cartItems							
+	}
+
 
 	def add(name:String,qty:Int):Boolean ={
-		val item = Item.get(name).get
-		val actualPrice = item.price(qty)
-		val discount = item.fullPrice(qty) - actualPrice
-		items = CartItem(item,qty,actualPrice,discount)::items
+		if(!Item.get(name).isDefined)
+			return false
+		items=addItem(name,qty)::items
+		applyOffer
 		true
 	}
 
-	def price = items.map(_.totalPrice).sum
+	def addItem(name:String,qty:Int):CartItem={
+		val item = Item.get(name).get
+		val actualPrice = item.price(qty)*cartDiscount
+		val discount = item.fullPrice(qty) - actualPrice
+		CartItem(item,qty,actualPrice,discount)
+	}
+
+	def checkout={
+		println(items)
+		println(price)
+	}
+
+	def discount = 100-(cartDiscount*100)
 }
+
+case class CartOffer(val criteria:(Cart)=>Boolean, val discount:Double)
+
+object CartOffer{
+	def fiveKPlus(cart:Cart)=cart.price>=500 
+
+	val offers:Set[CartOffer] = Set(CartOffer(fiveKPlus,20))	
+	def get(cart:Cart) = offers.filter(_.criteria(cart))
+}
+
